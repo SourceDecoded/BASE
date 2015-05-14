cfd73f015f6d0a30c154b8d42490f37696134bb2
Jared Barnes <jared.j.barnes@gmail.com>
2015-05-14 17:10:15 -0600

    Fixed Bug

--- project/BASE/data/PrimitiveHandler.js
+++ project/BASE/data/PrimitiveHandler.js
@@ -29,7 +29,7 @@
                     entity[key] = handler(dto[key]);
                 } else if (properties[key].type !== Array) {
                     entity[key] = dto[key];
-                } else if (dto[key] !== "undefined"){
+                } else if (typeof dto[key] !== "undefined"){
                     entity[key] = dto[key];
                 }
             });
