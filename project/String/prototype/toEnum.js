6589e26f86b1d99e55b70439b61c4a39fa2e3c47
Jared Barnes <jared.j.barnes@gmail.com>
2015-07-23 09:41:29 -0600

    Renamed variable.

--- project/String/prototype/toEnum.js
+++ project/String/prototype/toEnum.js
@@ -4,17 +4,17 @@
     String.prototype.toEnum = function (Type) {
         var keys = this.split(",");
 
-        var stringList = keys.filter(function(string) {
+        var intList = keys.filter(function(string) {
             return Type[string.trim()] == null ? false : true;
         }).map(function(string) {
             return Type[string.trim()];
         });
         
-        if (stringList.length === 0) {
+        if (intList.length === 0) {
             return 0;
         }
 
-        return stringList.reduce(function (last, next) {
+        return intList.reduce(function (last, next) {
             return last | next;
         });
     };
