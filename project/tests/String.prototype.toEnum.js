e01830e0260145e62e18c7fd988483b61c832328
Nora Valletta <nora.valletta@gmail.com>
2015-07-23 08:23:54 -0600

    Adds the String.prototype.toEnum test. I'm unable to run it as of now, since it isn't being detected as a test... but, you know, it's there for when I figure out how to get it to run.

---
+++ project/tests/String.prototype.toEnum.js
@@ -0,0 +1,34 @@
+﻿var assert = require("assert");
+
+require("../BASE.js");
+BASE.require.loader.setRoot("./");
+
+BASE.require([
+    "String.prototype.toEnum"
+], function () {
+    
+    exports["String.prototype.toEnum"] = function() {
+        PhoneNumberType = function() {};
+        PhoneNumberType.Object = {};
+        PhoneNumberType.String = "String";
+        PhoneNumberType.Number = 1;
+        PhoneNumberType.Null = null;
+        PhoneNumberType.Undefined = undefined;
+        PhoneNumberType.None = new Number(0);
+        PhoneNumberType.Home = new Number(1);
+        PhoneNumberType.Work = new Number(2);
+        PhoneNumberType.Mobile = new Number(4);
+        PhoneNumberType.Mobile2 = new Number(8);
+
+        PhoneNumberType.None.name = "None";
+        PhoneNumberType.Home.name = "Home";
+        PhoneNumberType.Work.name = "Work";
+        PhoneNumberType.Mobile.name = "Mobile";
+        PhoneNumberType.Mobile2.name = "Mobile2";
+
+        assert.equal("Home".toEnum(PhoneNumberType), (PhoneNumberType.Home));
+        assert.equal("Home, Work".toEnum(PhoneNumberType), (PhoneNumberType.Home | PhoneNumberType.Work));
+        assert.equal("Mobile2, Work, Home".toEnum(PhoneNumberType), (PhoneNumberType.Mobile2 | PhoneNumberType.Work | PhoneNumberType.Home));
+    };
+    
+});
