71433fbf30adeae0d16a5e8e372abc5f5da65ef1
Blake Plumb <plumbblake@gmail.com>
2015-08-04 09:15:53 -0600

    made invokeMethodIfExists

---
+++ project/BASE/util/invokeMethodIfExists.js
@@ -0,0 +1,18 @@
+﻿BASE.require([
+    "BASE.async.Future"
+], function () {
+    BASE.namespace('BASE.util');
+    
+    var Future = BASE.async.Future;
+    
+    BASE.util.invokeMethodIfExists = function (controller, methodName, args) {
+        var value;
+        if (!(controller instanceof Object)) {
+            throw new Error("A controller must be supplied");
+        }
+        if (typeof controller[methodName] === "function") {
+            value = controller[methodName].apply(controller, args);
+        }
+        return value;
+    }
+});
---
+++ project/tests/BASE.util.invokeMethodIfExists.js
@@ -0,0 +1,48 @@
+﻿var assert = require("assert");
+
+require("../BASE.js");
+BASE.require.loader.setRoot("./");
+
+BASE.require([
+    "BASE.util.invokeMethodIfExists",
+    "BASE.async.Future"
+], function () {
+    var Future = BASE.async.Future;
+    
+    var obj = {
+        methodReturnsValue: function () {
+            return "Some Value"
+        },
+        methodWithArguments: function (one, two, three) {
+            return Array.prototype.slice.call(arguments, 0);
+        },
+        methodReturnsFuture: function () {
+            return Future.fromResult("Value");
+        }
+    };
+    
+    var invokeMethodIfExists = BASE.util.invokeMethodIfExists;
+    
+    exports["BASE.util.invokeMethodIfExists: Success with Future returned."] = function () {
+        var result = invokeMethodIfExists(obj, "methodReturnsValue", [])
+        assert.equal(result, "Some Value");
+    };
+    
+    exports["BASE.util.invokeMethodIfExists: Success with Future."] = function () {
+        var result = invokeMethodIfExists(obj, "methodReturnsFuture", []).then(function (value) {
+            assert.equal(value, "Value");
+        });
+        assert.equal(result instanceof Future, true);
+    };
+    
+    exports["BASE.util.invokeMethodIfExists: Arguments are passed correctly."] = function () {
+        var one = "one";
+        var two = "two";
+        var three = "three";
+        
+        var result = invokeMethodIfExists(obj, "methodWithArguments", [one, two, three]);
+        assert.equal(result[0], one);
+        assert.equal(result[1], two);
+        assert.equal(result[2], three);
+    };
+});
