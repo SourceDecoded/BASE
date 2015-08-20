8aa06a2977f0d71536468629dadf17d704b7d71a
Jared Barnes <jared.j.barnes@gmail.com>
2015-08-20 15:39:51 -0600

    Fixed

--- project/BASE/web/animation/Animation.js
+++ project/BASE/web/animation/Animation.js
@@ -7,30 +7,30 @@
     "BASE.web.animation.animationStateManager",
     "BASE.web.animation.easings"
 ], function () {
-
+    
     BASE.namespace("BASE.web.animation");
-
+    
     var easings = BASE.web.animation.easings;
     var AnimationManager = BASE.web.animation.AnimationManager;
     var animationStateManager = BASE.web.animation.animationStateManager;
     var Future = BASE.async.Future;
-
+    
     var makeTickPercentageObservers = function (observers) {
         for (var x = 0; x <= 100 ; x++) {
             observers[x] = [];
         }
     };
-
+    
     var returnObserver = function (observer) {
         return observer;
     };
-
+    
     var Observer = function (callback, unbind) {
         this._callback = callback;
         this._currentState = Observer.prototype.stateManager.started;
         this.unbind = unbind || function () { };
     };
-
+    
     Observer.prototype.stateManager = {
         stopped: {
             start: function (observer) {
@@ -52,26 +52,26 @@
             }
         }
     };
-
+    
     Observer.prototype.stop = function () {
         return this._currentState.stop(this);
     };
-
+    
     Observer.prototype.start = function () {
         return this._currentState.start(this);
     };
-
+    
     Observer.prototype.callback = function (event) {
         return this._currentState.callback(this, event);
     };
-
+    
     Observer.prototype.dispose = function () {
         return this.unbind()
     };
-
+    
     var Animation = function (config) {
         config = config || {};
-
+        
         this._target = config.target || {};
         this._currentTime = 0;
         this._timeScale = 1;
@@ -82,11 +82,11 @@
         this._startTime = 0;
         this._currentRequestAnimationFrameId = null;
         this._currentState = animationStateManager.stoppedState;
-
+        
         this.iterations = 0;
         this.repeat = 1;
         this.repeatDirection = 0;
-
+        
         this._observers = {
             play: [],
             stop: [],
@@ -98,9 +98,9 @@
             end: [],
             start: []
         };
-
+        
         makeTickPercentageObservers(this._observers);
-
+        
         if (typeof config.easing === "string") {
             this._easingFunction = easings[config.easing];
         } else if (typeof config.easing === "function") {
@@ -110,123 +110,127 @@
         }
 
     };
-
+    
     Animation.prototype.animationManager = new AnimationManager();
-
+    
     Animation.prototype._saveBeginningValues = function () {
         var target = this._target;
         var beginningValues = this._beginningValues;
         var properties = this._properties;
-
+        
         Object.keys(properties).forEach(function (property) {
             beginningValues[property] = target[property];
         });
     };
-
+    
     Animation.prototype.play = function () {
         return this._currentState.play(this);
     };
-
+    
     Animation.prototype.stop = function () {
         this._currentState.stop(this);
         return this;
     };
-
+    
     Animation.prototype.observeAtTick = function (percentage, callback) {
         percentage = percentage * 100;
         if (typeof percentage === "number" && percentage >= 0 && percentage <= 100) {
             percentage = parseInt(percentage);
             return this.observe(percentage.toString(), callback);
         }
-
+        
         throw new Error("Invalid Argument Exception: percentage must be a decimal, and with in 0-1");
     };
-
+    
     Animation.prototype.playToEndAsync = function (startAt) {
         var self = this;
-
-        if (typeof startAt === "number" && startAt >=0 && startAt <=1) {
+        
+        if (typeof startAt === "number" && startAt >= 0 && startAt <= 1) {
             self._progress = startAt;
         }
-
+        
         return new Future(function (setValue, setError, cancel, ifCanceled) {
+            
+            self.stop();
 
             var disposeAllObservers = function () {
                 reverseObserver.dispose();
                 endObserver.dispose();
                 stopObserver.dispose();
             };
-
+            
             var endObserver = self.observe("end", function () {
                 disposeAllObservers();
                 setValue();
             });
-
+            
             var canceledCallback = function (event) {
                 disposeAllObservers();
                 cancel(event.type);
             };
-
+            
             var stopObserver = self.observe("stop", canceledCallback);
             var reverseObserver = self.observe("reverse", canceledCallback);
-
+            
             ifCanceled(function () {
                 self.stop();
             });
-
+            
             self.play();
         });
     };
-
+    
     Animation.prototype.reverseToStartAsync = function (startAt) {
         var self = this;
         
         if (typeof startAt === "number" && startAt >= 0 && startAt <= 1) {
             self._progress = startAt;
         }
-
+        
         return new Future(function (setValue, setError, cancel, ifCanceled) {
-
+            
+            self.stop();
+            
             var disposeAllObservers = function () {
                 startObserver.dispose();
                 stopObserver.dispose();
                 playObserver.dispose();
             };
-
+            
             var startObserver = self.observe("start", function () {
                 disposeAllObservers();
                 setValue();
             });
-
+            
             var canceledCallback = function (event) {
                 disposeAllObservers();
                 cancel(event.type);
             };
-
+            
             var stopObserver = self.observe("stop", canceledCallback);
             var playObserver = self.observe("play", canceledCallback);
-
+            
             ifCanceled(function () {
                 self.stop();
             });
-
+            
             self.reverse();
 
         });
     };
-
+    
     Animation.prototype.pause = function () {
         return this._currentState.pause(this);
     };
-
+    
     Animation.prototype.restart = function () {
         return this._currentState.restart(this);
     };
-
+    
     Animation.prototype.reverse = function () {
         return this._currentState.reverse(this);
     };
-
+    
     Animation.prototype.notify = function (event) {
         var type = event.type;
         if (Array.isArray(this._observers[type])) {
@@ -235,60 +239,60 @@
             });
         }
     };
-
+    
     Animation.prototype.tick = function (time) {
         var value = this._currentState.tick(this, time);
         return value;
     };
-
+    
     Animation.prototype.invalidate = function () {
         this._progress = 0;
         this._currentState = animationStateManager.pausedState;
         return this;
     };
-
+    
     Animation.prototype.getProgress = function () {
         return this._progress;
     };
-
+    
     Animation.prototype.setTimeScale = function (timeScale) {
         this._timeScale = timeScale;
     };
-
+    
     Animation.prototype.getTimeScale = function () {
         return this._timeScale;
     };
-
+    
     Animation.prototype.seek = function (progressValue, now) {
         this._currentState.seek(this, progressValue, now);
         return this;
     };
-
+    
     Animation.prototype.observe = function (type, callback) {
         var self = this;
-
+        
         if (typeof type !== "string") {
             throw new Error("Need to supply a type.");
         }
-
+        
         var callbacks = this._observers[type];
-
+        
         if (typeof callbacks === "undefined") {
             throw new Error("Unknown type to observe to. Here is a list of types to observe to: play, stop, pause, restart, reverse, seek, tick, end, start");
         }
-
+        
         var observer = new Observer(callback, function () {
             var index = callbacks.indexOf(observer);
             if (index >= 0) {
                 callbacks.splice(index, 1);
             }
         });
-
+        
         callbacks.push(observer);
-
+        
         return observer;
     };
-
+    
     Animation.prototype.render = function () {
         var self = this;
         var progress = this._progress;
@@ -303,47 +307,47 @@
         var endingValue;
         var property;
         var value;
-
+        
         for (property in properties) {
             //beginningValue, endingValue, currentTime, duration, easing
             var beginningValue = beginningValues[property];
             var endingValue = endingValues[property];
-
+            
             if (typeof endingValue === "object" && endingValue !== null) {
                 beginningValue = endingValue.from;
                 endingValue = endingValue.to;
             }
-
+            
             if (typeof beginningValue === "undefined") {
                 beginningValues[property] = target[property];
                 beginningValue = target[property];
             }
-
+            
             if (typeof beginningValue !== "number" || typeof endingValue !== "number") {
                 throw new Error("Default renderer is only able to animate integers. Set the renderer in the config to handle custom values.");
             }
-
+            
             var change = endingValue - beginningValue;
             var currentTime = progress * duration;
-
+            
             if (change !== 0) {
                 value = easingFunction(currentTime, beginningValue, change, duration);
             } else {
                 value = endingValue;
             }
-
+            
             // This will be more optimal. Don't set the value unless it changes.
             if (target[property] !== value) {
                 target[property] = value;
             }
         }
-
+        
         return this;
     };
-
+    
     Animation.REPEAT_DEFAULT = 0;
     Animation.REPEAT_ALTERATE = 1;
-
+    
     BASE.web.animation.Animation = Animation;
 
 });
