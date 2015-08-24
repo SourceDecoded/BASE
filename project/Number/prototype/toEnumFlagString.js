c9e6c91a60a7690a2da7aafe57a7b1751f445657
Jared Barnes <jared.j.barnes@gmail.com>
2015-08-24 15:55:00 -0600

    Fixed Enum stuff.

---
+++ project/Number/prototype/toEnumFlagString.js
@@ -0,0 +1,10 @@
+﻿Number.prototype.toEnumString = function (Type) {
+    var value = this.valueOf();
+    var response = Object.keys(Type).filter(function (key) {
+        return ((value & Type[key]) == Type[key]) && Type[key] != 0 && typeof Type[key].name === "string";
+    }).map(function (key) {
+        return Type[key].name || key;
+    }).join(", ");
+    
+    return (response.length > 0 ? response : "None");
+};
