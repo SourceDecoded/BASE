16c9136168867d0f2ecbff722461ea3373b0186c
Jared Barnes <jared.j.barnes@gmail.com>
2015-08-26 17:31:24 -0600

    Finished the where part of visitors.

---
+++ project/BASE/parse/Visitor.js
@@ -0,0 +1,231 @@
+﻿BASE.require([
+    "BASE.query.ExpressionVisitor",
+    "Array.prototype.indexOfByFunction"
+], function () {
+    BASE.namespace("BASE.parse");
+    
+    BASE.parse.Visitor = (function (Super) {
+        var Visitor = function (config) {
+            var self = this;
+            BASE.assertNotGlobal(self);
+            
+            Super.call(self);
+            config = self.config = (config || {});
+            self.scope = config.scope || "";
+            
+            if (typeof config.model === "undefined") {
+                throw new Error("Null Argument Exception: model cannot be undefined in configurations.");
+            }
+            
+            if (typeof config.edm === "undefined") {
+                throw new Error("Null Argument Exception: edm cannot be undefined in configurations.");
+            }
+            
+            var model = self.model = config.model || { properties: {} };
+            self.currentModel = self.model;
+            self.edm = config.edm;
+            
+            var superParse = self.parse;
+            self.parse = function () {
+                self.currentWhere = {};
+                self.currentExpression = self.currentWhere;
+                return superParse.apply(self, arguments);
+            };
+            
+            return self;
+        };
+        
+        BASE.extend(Visitor, Super);
+        
+        Visitor.prototype["isIn"] = function (propertyObject, array) {
+           
+        };
+        
+        Visitor.prototype["isNotIn"] = function (propertyObject, array) {
+           
+        };
+        
+        Visitor.prototype["ascending"] = function (propertyObject) {
+        };
+        
+        Visitor.prototype["descending"] = function (propertyObject) {
+        };
+        
+        Visitor.prototype["orderBy"] = function () {
+           
+        };
+        
+        Visitor.prototype["where"] = function (propertyAccess) {
+            return "where=" + JSON.stringify(this.currentWhere);
+        };
+        
+        Visitor.prototype["and"] = function () {
+            return this.currentWhere;
+        };
+        
+        Visitor.prototype["or"] = function () {
+            var ors = Array.prototype.slice.call(arguments, 0);
+            
+            if (!this.currentWhere.hasOwnProperty("$or")) {
+                this.currentWhere = { "$or": ors };
+            } else {
+                this.currentWhere["$or"].concat(ors);
+            }
+            
+            return this.currentWhere;
+        };
+        
+        Visitor.prototype["equalTo"] = function (propertyAccess, value) {
+            propertyAccess.currentExpression[propertyAccess.property] = value;
+            return this.currentExpression;
+        };
+        
+        Visitor.prototype["notEqualTo"] = function (propertyAccess, value) {
+            if (typeof propertyAccess.propertyObject === "object" && propertyAccess.propertyObject !== null) {
+                propertyAccess.propertyObject["$ne"] = value;
+            }
+            return this.currentExpression;
+        };
+        
+        Visitor.prototype["greaterThan"] = function (propertyAccess, value) {
+            if (typeof propertyAccess.propertyObject === "object" && propertyAccess.propertyObject !== null) {
+                propertyAccess.propertyObject["$gt"] = value;
+            }
+            return this.currentExpression;
+        };
+        
+        Visitor.prototype["lessThan"] = function (propertyAccess, value) {
+            if (typeof propertyAccess.propertyObject === "object" && propertyAccess.propertyObject !== null) {
+                propertyAccess.propertyObject["$lt"] = value;
+            }
+            return this.currentExpression;
+        };
+        
+        Visitor.prototype["greaterThanOrEqualTo"] = function (propertyAccess, value) {
+            if (typeof propertyAccess.propertyObject === "object" && propertyAccess.propertyObject !== null) {
+                propertyAccess.propertyObject["$gte"] = value;
+            }
+            return this.currentExpression;
+        };
+        
+        Visitor.prototype["lessThanOrEqualTo"] = function (propertyAccess, value) {
+            if (typeof propertyAccess.propertyObject === "object" && propertyAccess.propertyObject !== null) {
+                propertyAccess.propertyObject["$lte"] = value;
+            }
+            return this.currentExpression;
+        };
+        
+        Visitor.prototype["substringOf"] = function (propertyAccess, value) {
+            if (typeof propertyAccess.propertyObject === "object" && propertyAccess.propertyObject !== null) {
+                propertyAccess.propertyObject["$regex"] = value.split("").reduce(function (acc, value) {
+                    return acc + "[" + value + "]";
+                }, "");
+            }
+            return this.currentExpression;
+        };
+        
+        Visitor.prototype["startsWith"] = function (propertyAccess, value) {
+            if (typeof propertyAccess.propertyObject === "object" && propertyAccess.propertyObject !== null) {
+                propertyAccess.propertyObject["$regex"] = "^" + value;
+            }
+            return this.currentExpression;
+        };
+        
+        Visitor.prototype["endsWith"] = function (propertyAccess, value) {
+            if (typeof propertyAccess.propertyObject === "object" && propertyAccess.propertyObject !== null) {
+                propertyAccess.propertyObject["$regex"] = value + "$";
+            }
+            return this.currentExpression;
+        };
+        
+        
+        Visitor.prototype["constant"] = function (expression) {
+            return expression.value;
+        };
+        
+        Visitor.prototype["property"] = function (expression) {
+            return expression.value;
+        };
+        
+        Visitor.prototype["propertyAccess"] = function (propertyAccess, property) {
+            BASE.namespace(property, propertyAccess.currentExpression);
+            var propertyObject = BASE.getObject(property, propertyAccess.currentExpression);
+            
+            return {
+                property: property,
+                propertyObject: propertyObject,
+                currentExpression: propertyAccess.currentExpression
+            };
+        };
+        
+        Visitor.prototype["type"] = function (type) {
+            return {
+                property: "",
+                propertyObject: {},
+                currentExpression: this.currentExpression
+            };
+        };
+        
+        Visitor.prototype["null"] = function (expression) {
+            return expression.value;
+        };
+        
+        Visitor.prototype["object"] = function (expression) {
+            return expression.value;
+        };
+        
+        Visitor.prototype["undefined"] = function (expression) {
+            return null;
+        };
+        
+        Visitor.prototype["date"] = function (expression) {
+            return expression.value;
+        };
+        
+        Visitor.prototype["string"] = function (expression) {
+            return expression.value;
+        };
+        
+        Visitor.prototype["number"] = function (expression) {
+            return expression.value;
+        };
+        
+        Visitor.prototype["boolean"] = function (expression) {
+            return expression.value;
+        };
+        
+        Visitor.prototype["all"] = function (propertyObject, expression) {
+          
+        };
+        
+        Visitor.prototype["any"] = function (propertyObject, expression) {
+         
+        };
+        
+        Visitor.prototype["expression"] = function (expression) {
+        
+        };
+        
+        Visitor.prototype["array"] = function (expression) {
+
+        }
+        
+        Visitor.prototype["has"] = function (propertyObject, value) {
+
+        };
+        
+        Visitor.prototype["not"] = function (expression) {
+
+        };
+        
+        Visitor.prototype["skip"] = function (value) {
+
+        };
+        
+        Visitor.prototype["take"] = function (value) {
+
+        };
+        
+        return Visitor;
+    }(BASE.query.ExpressionVisitor));
+});
---
+++ project/tests/BASE.parse.Visitor.js
@@ -0,0 +1,172 @@
+﻿var assert = require("assert");
+
+require("../BASE.js");
+BASE.require.loader.setRoot("./");
+
+BASE.require([
+    "BASE.query.Queryable",
+    "BASE.parse.Visitor",
+    "BASE.data.testing.Edm",
+    "BASE.data.testing.model.person"
+], function () {
+    
+    var Visitor = BASE.parse.Visitor;
+    var Queryable = BASE.query.Queryable;
+    var Edm = BASE.data.testing.Edm;
+    var personModel = BASE.data.testing.model.person;
+    
+    var config = {
+        model: personModel,
+        edm: new Edm()
+    };
+    
+    exports["BASE.parse.Visitor: Orderby"] = function () {
+      
+
+    };
+    
+    exports["BASE.parse.Visitor: OrderbyDesc"] = function () {
+      
+
+    };
+    
+    exports["BASE.parse.Visitor: Multiple OrderbyDesc"] = function () {
+       
+
+    };
+    
+    exports["BASE.parse.Visitor: Filter isEqualTo"] = function () {
+        var correctValue = 'where={"age":1000}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("age").isEqualTo(1000);
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: Filter isNotEqualTo"] = function () {
+        var correctValue = 'where={"age":{"$ne":1000}}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("age").isNotEqualTo(1000);
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: or"] = function () {
+        var correctValue = 'where={"$or":[{"age":{"$ne":1000}},{"firstName":{"$ne":"Jared"}}]}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.or(
+                expBuilder.property("age").isNotEqualTo(1000),
+                expBuilder.property("firstName").isNotEqualTo("Jared")
+            );
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: Filter isGreaterThan"] = function () {
+        var correctValue = 'where={"age":{"$gt":1000}}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("age").isGreaterThan(1000);
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: Filter isGreaterThanOrEqualTo"] = function () {
+        var correctValue = 'where={"age":{"$gte":1000}}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("age").isGreaterThanOrEqualTo(1000);
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: Filter isLessThan"] = function () {
+        var correctValue = 'where={"age":{"$lt":1000}}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("age").isLessThan(1000);
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: Filter isLessThanOrEqualTo"] = function () {
+        var correctValue = 'where={"age":{"$lte":1000}}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("age").isLessThanOrEqualTo(1000);
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: Filter contains"] = function () {
+        var correctValue = 'where={"firstName":{"$regex":"[J][a][r][e][d]"}}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("firstName").contains("Jared");
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: Filter startsWith"] = function () {
+        var correctValue = 'where={"firstName":{"$regex":"^Jared"}}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("firstName").startsWith("Jared");
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+    
+    exports["BASE.parse.Visitor: Filter endsWith"] = function () {
+        var correctValue = 'where={"firstName":{"$regex":"Jared$"}}';
+        
+        var queryable = new Queryable().where(function (expBuilder) {
+            return expBuilder.property("firstName").endsWith("Jared");
+        });
+        
+        var visitor = new Visitor(config);
+        var result = visitor.parse(queryable.getExpression().where);
+        
+        assert.equal(result, correctValue);
+    };
+
+    
+});
