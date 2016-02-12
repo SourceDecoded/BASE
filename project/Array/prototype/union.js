73c44466a247af26b8ebac31fa26a3f4e20e2669
Aaron Drabeck <aaron-drabeck@leavitt.com>
2016-02-11 17:05:11 -0700

    Safely calls defineProperty for older browsers

--- project/Array/prototype/union.js
+++ project/Array/prototype/union.js
@@ -1,22 +1,31 @@
-﻿BASE.require(["BASE.collections.Hashmap"], function () {
+﻿BASE.require(["BASE.collections.Hashmap"], function() {
     var Hashmap = BASE.collections.Hashmap;
-    
+
     if (!Array.prototype.union) {
-        Object.defineProperty(Array.prototype, "union", {
+        var value = {
             enumerable: false,
             configurable: true,
-            value: function (array) {
-                
+            value: function(array) {
+
                 var hashmap = new Hashmap();
-                var add = function (value) {
+                var add = function(value) {
                     hashmap.add(value, value);
                 };
-                
+
                 this.forEach(add);
                 array.forEach(add);
-                
+
                 return hashmap.getValues();
             }
-        });
+        };
+
+
+        if (Object.defineProperty) {
+            try {
+                Object.defineProperty(Array.prototype, "union", value);
+            } catch (e) {
+            };
+        }
+        if (!Array.prototype.union) Array.prototype.union = value;
     }
 });
