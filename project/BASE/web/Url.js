c7aecc88ac362fec1eea0b9344911621d72001c5
Jared Barnes <jared.j.barnes@gmail.com>
2016-05-12 16:58:32 -0600

    Fixed Url to handle partially encoded urls. Fixed some obscure bugs as well in the datacontext surrounding primitive types.

--- project/BASE/web/Url.js
+++ project/BASE/web/Url.js
@@ -1,26 +1,43 @@
 ﻿BASE.require([
     "BASE.web.queryString"
 ], function () {
-
+    
     BASE.namespace("BASE.web");
-
+    
     BASE.web.Url = function (url) {
         var self = this;
-
+        
         BASE.assertNotGlobal(self);
-
+        
         //Thanks Douglas Crockford.
         var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
         url = url || "";
+        
+        //We need to decoded until there are not changes detected.
+        var decodedUrl;
+        try {
+            decodedUrl = decodeURI(url);
+        } catch (e) { }
+        
+        while (decodedUrl !== url) {
+            url = decodedUrl;
+            try {
+                decodedUrl = decodeURI(url);
+            } catch (e) { }
+        }
+        url = decodedUrl;
+        
+        // Done Trying to decode.
+        
         var result = parse_url.exec(url);
-
+        
         if (result === null) {
             result = [];
         }
-
+        
         var parseQuery = BASE.web.queryString.parse;
         var stringify = BASE.web.queryString.toString;
-
+        
         var scheme = result[1];
         var slash = result[2];
         var host = result[3];
@@ -28,14 +45,15 @@
         var path = result[5];
         var query = result[6];
         var hash = result[7] || "";
-        var queryStringValues = parseQuery(result[6]);
-
+        var queryStringValues = parseQuery(query);
+        query = stringify(queryStringValues, false);
+        
         if (typeof port === "undefined") {
             port = (scheme === "https") ? 443 : 80;
         } else {
             port = parseInt(port, 10);
         }
-
+        
         self.getHref = function () {
             var schemeString = scheme || "http";
             var hostString = host || "";
@@ -44,72 +62,72 @@
             var hashString = hash ? "#" + encodeURIComponent(hash) : "";
             var pathString = path ? "/" + encodeURI(path) : "";
             var queryString = query ? "?" + query : "";
-
+            
             return schemeString + ":" + slashString + hostString + portString + pathString + queryString + hashString;
         };
-
+        
         self.getScheme = function () {
             return scheme;
         };
-
+        
         self.setScheme = function (value) {
             scheme = value;
         };
-
+        
         self.getSlash = function () {
-            return slash;
+            return decodeURI(slash);
         };
-
+        
         self.setSlash = function (value) {
             slash = value;
         };
-
+        
         self.getHost = function () {
             return host;
         };
-
+        
         self.setHost = function (value) {
             host = value;
         };
-
+        
         self.getPort = function () {
             return port;
         };
-
+        
         self.setPort = function (value) {
             port = value;
         };
-
+        
         self.getPath = function () {
             return decodeURI(path);
         };
-
+        
         self.setPath = function (value) {
             path = value;
         };
-
+        
         self.getQuery = function () {
-            return query;
+            return decodeURI(query);
         };
-
+        
         self.setQuery = function (obj) {
             queryStringValues = obj;
             query = stringify(obj).substr(1);
         };
-
+        
         self.getParsedQuery = function () {
             return queryStringValues;
         }
-
+        
         self.getHash = function () {
-
+            
             return decodeURIComponent(hash);
         };
-
+        
         self.setHash = function (value) {
             hash = value;
         };
-
+        
         self.getPage = function () {
             var tmpArray;
             if (path) {
@@ -119,7 +137,7 @@
                 return undefined;
             }
         };
-
+        
         self.getExtension = function () {
             var page = self.getPage();
             if (page) {
@@ -130,7 +148,7 @@
                 return undefined;
             }
         };
-
+        
         self.toString = self.getHref;
 
     };
--- project/BASE/web/queryString.js
+++ project/BASE/web/queryString.js
@@ -1,35 +1,59 @@
 ﻿BASE.require([], function () {
-
+    
     BASE.namespace("BASE.web.queryString");
+    
+    var decode = function (value) {
+        var decodedValue;
+        try {
+            decodedValue = decodeURIComponent(value);
+        } catch (e) { }
 
+        while (decodedValue !== value) {
+            value = decodedValue;
+            try {
+                decodedValue = decodeURIComponent(value);
+            } catch (e) { }
+        }
+        value = decodedValue;
+        return value;
+    };
+    
     BASE.web.queryString.toString = function (obj, includeQuestionMark) {
         if (typeof includeQuestionMark === "undefined") {
             includeQuestionMark = true;
         }
-
+        
         var parts = Object.keys(obj).map(function (key) {
-            if (Array.isArray(obj[key])) {
+            var value = obj[key];
+            
+            if (Array.isArray(value)) {
                 return obj[key].map(function (value) {
+                    value = decode(value);
+                    key = decode(key);
+                    
                     return encodeURIComponent(key) + "=" + encodeURIComponent(value);
                 }).join("&");
             } else {
-                return encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]);
+                value = decode(value);
+                key = decode(key);
+                
+                return encodeURIComponent(key) + "=" + encodeURIComponent(value);
             }
         });
         return (includeQuestionMark ? "?" : "") + parts.join("&");
     };
-
+    
     BASE.web.queryString.parse = function (querystring) {
         var values = {};
-
+        
         if (querystring) {
-
+            
             if (querystring.indexOf("?") === 0) {
                 querystring = querystring.substr(1);
             }
-
-            querystring = decodeURI(querystring);
-
+            
+            querystring = decode(querystring);
+            
             var keyValues = querystring.split("&");
             keyValues.forEach(function (keyValue) {
                 var split = keyValue.split("=");
