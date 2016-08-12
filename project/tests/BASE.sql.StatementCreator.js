4405d674a1958a04dd84bc7a7bb546e828135a1a
Jared Barnes <Jared Barnes>
2016-08-12 11:44:35 -0600

    Making more updates to sql optimizations.

---
+++ project/tests/BASE.sql.StatementCreator.js
@@ -0,0 +1,48 @@
+﻿var assert = require("assert");
+
+require("../BASE.js");
+BASE.require.loader.setRoot("./");
+
+BASE.require([
+    "BASE.sql.StatementCreator",
+    "BASE.data.testing.Edm"
+], function () {
+
+    var StatementCreator = BASE.sql.StatementCreator;
+    var Edm = BASE.data.testing.Edm;
+    var Person = BASE.data.testing.Person;
+
+    var edm = new Edm();
+
+    var isMatch = function (message) {
+        return function (error) {
+            return message === error.message;
+        };
+    };
+
+    exports["BASE.sql.StatementCreator: Create table."] = function () {
+        var edm = new Edm();
+        var personModel = edm.getModelByType(Person);
+        var statementCreator = new StatementCreator(edm);
+        var createStatement = statementCreator.createTableClause(personModel);
+
+        assert.equal(createStatement, "CREATE TABLE people(\n\tid INTEGER PRIMARY KEY AUTOINCREMENT, \n\tfirstName TEXT, \n\tlastName TEXT, \n\tage INTEGER, \n\tdateOfBirth NUMERIC, \n\thumanoidType NUMERIC\n)");
+
+    };
+
+    exports["BASE.sql.StatementCreator: Insert statement table."] = function () {
+        var edm = new Edm();
+
+        var person = new Person();
+        person.firstName = "Jared";
+        person.lastName = "Barnes";
+        person.dateOfBirth = new Date();
+        person.humanoidType = 1;
+
+        var statementCreator = new StatementCreator(edm);
+        var insertStatement = statementCreator.createInsertStatement(person);
+
+    };
+
+
+});
